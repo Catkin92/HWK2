7be13b0919aa30dd20cb73f09d0f08e2085da7d6
Yousef <yahmed1996a@gmail.com>
2019-10-15 15:38:24 +0100

    9th function tested

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -17,3 +17,15 @@ end
 def divide(no1,no2)
   results = no1 / no2
 end
+
+def length_of_string(string)
+  result = string.length
+end
+
+def join_string(string1, string2)
+  result = string1 + string2
+end
+
+def add_string_as_number(no1,no2)
+  results = no1.to_i + no2.to_i
+end
